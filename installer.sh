3f60758b8774d1c4abb47d041890c0e937b7a566
Pronged Fork <67598703+rapradab@users.noreply.github.com>
2021-08-27 21:09:34 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,3 +7,9 @@ apt-get update
 mkdir -p tools
 mkdir -p recon
 
+#Installing Aquatone for taking screenshots
+cd /root/tools/
+wget https://github.com/michenriksen/aquatone/releases/download/v1.4.3/aquatone_linux_amd64_1.4.3.zip
+unzip aquatone_linux_amd64_1.4.3.zip
+mv aquatone /usr/local/bin/
+rm aquatone_linux_amd64_1.4.3.zip LICENSE.txt README.md
